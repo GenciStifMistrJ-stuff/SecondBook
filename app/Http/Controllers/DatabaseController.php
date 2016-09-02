2dc4124dae4b922e0cbc0b62c713c6117a6c854f
StefanPruzinsky <pruzinsky.s@gmail.com>
2016-09-03 00:14:25 +0200

    Stuff

--- app/Http/Controllers/DatabaseController.php
+++ app/Http/Controllers/DatabaseController.php
@@ -1,18 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use View;
+use DB;
 
 class DatabaseController extends Controller
 {
     public function FetchData($table, $id, $column)
     {
-        $databaseStuff = $column.' -> Jano Peterson';
+        $databaseStuff = User::all();//DB::table('users')->where('id', '=', 1)->orWhere('id', '=', 3)->select(['name', 'password'])->get();//User::all()
 
-        return View::make('database')->with('stuff', $databaseStuff);
+
+        return View::make('database')->with('databaseStuff', $databaseStuff);
     }
 }
